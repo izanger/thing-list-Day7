20fbc97f67d22b04b7eb799d8a8dddecbdf484bb
Ian Zanger <izanger@purdue.edu>
2017-05-25 10:05:30 -0400

    Create SignIn and SignOut components

---
+++ src/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SignIn.css'
+
+const SignIn = () => {
+    return (
+        <button className="SignIn">
+            Sign In With Github
+        </button>    
+    )
+}
+export default SignIn
---
+++ src/SignOut.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SignOut.css'
+
+const SignOut = () => {
+    return (
+        <button className="SignOut">
+            Sign Out
+        </button>    
+    )
+}
+export default SignOut
